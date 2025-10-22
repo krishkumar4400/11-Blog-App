5a69e9632ba53a3e8c5617fdf278d19e38dc68f5
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-10-22 19:10:44 +0530

    bloglist  component completed

--- Frontend/src/components/BlogList.jsx
+++ Frontend/src/components/BlogList.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import BlogCard from "./BlogCard";
+import { blogCategories } from "../assets/assets";
+import {motion, spring} from 'motion/react';
 
 const BlogList = () => {
-  const categories = [
-    {name: 'All',},
-    {name: 'Technology',},
-    {name: 'Hardware',},
-    {name: 'Startup',},
-    {name: 'Lifestyle',},
-    {name: 'Finance',},
-  ];
-  
+  const [menu, setMenu] = useState("All");
+
   return (
-    <div>
-      <div className="inline-flex items-center gap-4 px-4 mb-10">
-        {categories.map((category, index) => (
-          <button
-            className="px-5 py-1.5 text-gray-700 rounded-full border-0 "
+    <div className="mt-8">
+      {/* Category Buttons */}
+      <div className="flex items-center justify-center gap-3 px-4 mb-10 relative flex-wrap">
+        {blogCategories.map((category, index) => (
+          <motion.div
+          transition={{type:spring, stiffness: 500, damping:30}}
+            onClick={() => setMenu(category)}
             key={index}
+            className={`px-6 py-2 text-sm font-medium rounded-full border border-gray-300 transition-all duration-200 relative cursor-pointer
+              ${
+                menu === category
+                  ? "bg-blue-600 text-white shadow-md"
+                  : "text-gray-600 hover:bg-gray-200"
+              }`}
           >
-            {category.name}
-          </button>
+            {category}
+          </motion.div>
         ))}
+      </div>
+
+      {/* Blog Cards */}
+      <div>
+        <BlogCard category={menu} />
       </div>
     </div>
   );
-}
+};
 
-export default BlogList
+export default BlogList;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       
       <Navbar/>
       <Hero/>
-      {/* <BlogList/> */}
+      <BlogList/>
     </div>
   )
 }
