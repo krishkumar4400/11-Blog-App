87b587c84aa5a2d696f5caf59fce74731a0dc105
krishkumar4400 <krishkumarbgp2022@gmail.com>
2025-10-24 02:20:31 +0530

    blog page

--- Frontend/src/pages/BlogPage.jsx
+++ Frontend/src/pages/BlogPage.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { blog_data } from '../assets/assets';
+import { assets, blog_data } from '../assets/assets';
+import Navbar from '../components/Navbar';
 
 const BlogPage = () => {
   const {id} = useParams();
-  console.log(id)
-  return (
-    <div>
-      {
-        blog_data.find(id)
-      }
+  const [data, setData] = useState(null);
+
+  const fetchBlogData = async () => {
+    const data = blog_data.find(blog => blog._id === id);
+    setData(data);
+  }
+
+  useEffect(() => {
+    fetchBlogData();
+  }, [data]);
+
+  return data ? (
+    <div className='relative'>
+      <img src={assets.gradientBackground} alt="" className='absolute -top-50 -z-1 opacity-50' />
+      <Navbar/>
+      <div></div>
+      <div></div>
     </div>
-  )
+  ) : <div>Loading...</div>
 }
 
 export default BlogPage
